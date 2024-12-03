eea8a44d0af6d67571c42f143cae1696864e7813
Arga Adinata <argaadinata10@gmail.com>
2024-12-03 15:03:06 +0700

    bugfix 12

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Transaksi;
+use App\Models\TransaksiDetail;
+
 class DashboardController extends Controller
 {
     public function index()
     {
-        $transaksi_count =
-        return view('dashboard', );
+        $transaksi_count = Transaksi::count();
+        $item_count = TransaksiDetail::sum('jumlah');
+        $omzet = Transaksi::sum('total_harga');
+
+        return view('dashboard', compact('transaksi_count', 'item_count', 'omzet'));
     }
 }
